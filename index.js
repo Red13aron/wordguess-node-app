cbf5adb1856795022a975fbf7ef1b892a26a3298
SNelson <snelson1337@gmail.com>
2019-09-17 10:22:31 -0500

    Added color and figlet to the game

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const inquirer = require("inquirer");
 const Word = require("./Word");
+const figlet = require("figlet");
+const colors = require("colors");
+
 const libraryWords = [`Fishhook`,
     `Rhythmic`, `Numbskull`, `Wildebeest`,
     `Phlegm`, `Dwarves`, `Sphinx`];
@@ -14,13 +17,25 @@ for (let i = 0; i < libraryWords.length; i++) {
 }
 let guessesLeft = 10;
 
+
+function figletize(string){
+    figlet(string, function (err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+    });
+}
+
 //sets up the game
 function setUp() {
     inquirer.
         prompt([
             {
                 name: "again",
-                message: "Would you like to play again? Y/N",
+                message: "Would you like to play again? Y/N".green,
                 type: "input"
             }
         ])
@@ -37,8 +52,15 @@ function setUp() {
                 lettersGuessed = [];
                 wordGame();
             }
-            else{
-                console.log(`Well it was fun while it lasted...`)
+            else {
+                figlet(`Well it was fun while it lasted...`, function (err, data) {
+                    if (err) {
+                        console.log('Something went wrong...');
+                        console.dir(err);
+                        return;
+                    }
+                    console.log(data)
+                });
             }
         });
 
@@ -47,12 +69,12 @@ function setUp() {
 
 function wordGame() {
     // Prompt for a Letter
-    console.log(`Here's the current word: ${currWord.myWord()}`);
+    console.log(`Here's the current word: ${currWord.myWord()}`.blue);
     inquirer
         .prompt([
             {
                 name: "guess",
-                message: "What letter would you like to guess?",
+                message: "What letter would you like to guess?".blue,
                 type: "input",
 
             }
@@ -65,11 +87,12 @@ function wordGame() {
                 }
             }
             if (currWord.checkLetter(answer.guess) === true && !isGuessed) {
-                console.log(`You got one! ${answer.guess} was correct!`);
+
+                console.log(`You got one! ${answer.guess} was correct!`.green);
                 lettersGuessed.push(answer.guess);
                 currWord.myWord();
                 if (currWord.myWord().indexOf(`_`) === -1) {
-                    console.log(`Congratulations! You guessed the word ${currWord.myWord()}!`);
+                    console.log(`Congratulations! You guessed the word ${currWord.myWord()}!`.green);
                     setUp();
                 }
                 else {
@@ -77,17 +100,17 @@ function wordGame() {
                 }
 
             }
-            else if(isGuessed === true){
-                console.log(`Please guess a different letter.  I won't count this one ;D`);
+            else if (isGuessed === true) {
+                console.log(`Please guess a different letter.  I won't count this one ;D`.red);
                 wordGame();
             }
             else {
-                console.log(`I'm sorry ${answer.guess} isn't in this word`);
+                console.log(`I'm sorry ${answer.guess} isn't in this word`.red);
                 lettersGuessed.push(answer.guess);
                 guessesLeft--;
-                console.log(`You have ${guessesLeft} guesses left.`);
+                console.log(`You have ${guessesLeft} guesses left.`.red);
                 if (guessesLeft === 0) {
-                    console.log(`I'm sorry you've run out of guesses :C`);
+                    console.log(`I'm sorry you've run out of guesses :C`.red);
                     setUp();
                 }
                 else {
